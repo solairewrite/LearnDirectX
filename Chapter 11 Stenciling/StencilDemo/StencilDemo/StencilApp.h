67124ec79ad84a509d908ea278878e42994f25f3
solairewrite <1029509407@qq.com>
2019-06-11 15:03:25 +0800

    StencilApp.h

--- Chapter 11 Stenciling/StencilDemo/StencilDemo/StencilApp.h
+++ Chapter 11 Stenciling/StencilDemo/StencilDemo/StencilApp.h
@@ -1,7 +1,3 @@
-
-
-
-
 #include "../../../Common/d3dApp.h"
 #include "../../../Common/MathHelper.h"
 #include "../../../Common/UploadBuffer.h"
@@ -17,34 +13,22 @@ using namespace DirectX::PackedVector;
 
 const int gNumFrameResources = 3;
 
-
-
 struct RenderItem
 {
 	RenderItem() = default;
-
-
-
 
 	XMFLOAT4X4 World = MathHelper::Identity4x4();
 
 	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
 
-
-
-
-
 	int NumFramesDirty = gNumFrameResources;
-
 
 	UINT ObjCBIndex = -1;
 
 	Material* Mat = nullptr;
 	MeshGeometry* Geo = nullptr;
 
-
 	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
-
 
 	UINT IndexCount = 0;
 	UINT StartIndexLocation = 0;
@@ -53,7 +37,7 @@ struct RenderItem
 
 enum class RenderLayer : int
 {
-	Opaque=0,
+	Opaque = 0,
 	Mirrors,
 	Reflected,
 	Transparent,
@@ -74,4 +58,77 @@ public:
 private:
 	virtual void OnResize() override;
 	virtual void Update(const GameTimer& gt) override;
+	virtual void Draw(const GameTimer& gt) override;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y) override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y) override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y) override;
+
+	void OnKeyboardInput(const GameTimer& gt);
+	void UpdateCamera(const GameTimer& gt);
+	void AnimateMaterials(const GameTimer& gt);
+	void UpdateObjectCBs(const GameTimer& gt);
+	void UpdateMaterialCBs(const GameTimer& gt);
+	void UpdateMainPassCB(const GameTimer& gt);
+	void UpdateReflectedPassCB(const GameTimer& gt);
+
+	void LoadTextures();
+	void BuildRootSignature();
+	void BuildDescriptorHeaps();
+	void BuildShadersAndInputLayout();
+	void BuildRoomGeometry();
+	void BuildSkullGeometry();
+	void BuildPSOs();
+	void BuildFrameResources();
+	void BuildMaterials();
+	void BuildRenderItems();
+	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
+
+	std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers();
+
+private:
+
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+	FrameResource* mCurrFrameResource = nullptr;
+	int mCurrFrameResourceIndex = 0;
+
+	UINT mCbvSrvDescriptorSize = 0;
+
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+
+	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
+
+	// Cache render items for interest
+	RenderItem* mSkullRitem = nullptr;
+	RenderItem* mReflectedSkullRitem = nullptr;
+	RenderItem* mShadowedSkullRitem = nullptr;
+
+	// List of all the render items
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+
+	// Render items divided by PSO
+	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];
+
+	PassConstants mMainPassCB;
+	PassConstants mReflectedPassCB;
+
+	XMFLOAT3 mSkullTranslation = { 0.0f, 1.0f, -5.0f };
+
+	XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+	XMFLOAT4X4 mView = MathHelper::Identity4x4();
+	XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+
+	float mTheta = 1.24f*XM_PI;
+	float mPhi = 0.42f*XM_PI;
+	float mRadius = 12.0f;
+
+	POINT mLastMousePos;
 };
