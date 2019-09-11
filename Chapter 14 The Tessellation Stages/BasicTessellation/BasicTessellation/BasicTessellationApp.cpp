5609ffa35d91e08aa9b995c584c6a12b4c9ce2b2
solairewrite <1029509407@qq.com>
2019-09-11 10:58:36 +0800

    曲面细分notes

--- Chapter 14 The Tessellation Stages/BasicTessellation/BasicTessellation/BasicTessellationApp.cpp
+++ Chapter 14 The Tessellation Stages/BasicTessellation/BasicTessellation/BasicTessellationApp.cpp
@@ -509,6 +509,7 @@ void BasicTessellationApp::BuildPSOs()
 	};
 	opaquePsoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
 	opaquePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
+	opaquePsoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
 	opaquePsoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
 	opaquePsoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
 	opaquePsoDesc.SampleMask = UINT_MAX;
