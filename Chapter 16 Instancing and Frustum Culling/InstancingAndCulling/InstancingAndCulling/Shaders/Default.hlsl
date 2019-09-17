73371f0c319bc20822905b523d6ad7b094faacf5
solairewrite <1029509407@qq.com>
2019-09-17 10:34:23 +0800

    finish ch16 实例化与视锥体剔除

--- Chapter 16 Instancing and Frustum Culling/InstancingAndCulling/InstancingAndCulling/Shaders/Default.hlsl
+++ Chapter 16 Instancing and Frustum Culling/InstancingAndCulling/InstancingAndCulling/Shaders/Default.hlsl
@@ -82,14 +82,14 @@ struct VertexOut
     float3 NormalW : NORMAL;
     float2 TexC : TEXCOORD;
 
-    nointerpolation uint MatIndex : MATINDEX; // nointerpolation: 该索引指向的都是未经插值的三角形
+    nointerpolation uint MatIndex : MATINDEX; // 该索引指向的都是未经差值的三角形
 };
 
-VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID) // SV_InstanceID: 每个实例的所有顶点编号都为instanceID
+// SV_InstanceID: 每个实例的所有顶点的编号都为 instanceID
+VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID)
 {
     VertexOut vout = (VertexOut) 0.0f;
-	
-	// Fetch the instance data.
+
 	// 获取实例数据
     InstanceData instData = gInstanceData[instanceID];
     float4x4 world = instData.World;
@@ -98,30 +98,24 @@ VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID) // SV_InstanceID: 每
 
     vout.MatIndex = matIndex;
 	
-	// Fetch the material data.
 	// 获取材质数据
     MaterialData matData = gMaterialData[matIndex];
 	
-    // Transform to world space.
     float4 posW = mul(float4(vin.PosL, 1.0f), world);
     vout.PosW = posW.xyz;
-
-    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+	
     vout.NormalW = mul(vin.NormalL, (float3x3) world);
-
-    // Transform to homogeneous clip space.
+	
     vout.PosH = mul(posW, gViewProj);
 	
-	// Output vertex attributes for interpolation across triangle.
     float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), texTransform);
     vout.TexC = mul(texC, matData.MatTransform).xy;
-	
+
     return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-	// Fetch the material data.
 	// 获取材质数据
     MaterialData matData = gMaterialData[pin.MatIndex];
     float4 diffuseAlbedo = matData.DiffuseAlbedo;
@@ -129,27 +123,22 @@ float4 PS(VertexOut pin) : SV_Target
     float roughness = matData.Roughness;
     uint diffuseTexIndex = matData.DiffuseMapIndex;
 	
-	// Dynamically look up the texture in the array.
     diffuseAlbedo *= gDiffuseMap[diffuseTexIndex].Sample(gsamLinearWrap, pin.TexC);
 	
-    // Interpolating normal can unnormalize it, so renormalize it.
     pin.NormalW = normalize(pin.NormalW);
-
-    // Vector from point being lit to eye. 
+	
     float3 toEyeW = normalize(gEyePosW - pin.PosW);
-
-    // Light terms.
+	
     float4 ambient = gAmbientLight * diffuseAlbedo;
 
     const float shininess = 1.0f - roughness;
     Material mat = { diffuseAlbedo, fresnelR0, shininess };
     float3 shadowFactor = 1.0f;
     float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
-        pin.NormalW, toEyeW, shadowFactor);
+		pin.NormalW, toEyeW, shadowFactor);
 
     float4 litColor = ambient + directLight;
 
-    // Common convention to take alpha from diffuse albedo.
     litColor.a = diffuseAlbedo.a;
 
     return litColor;
